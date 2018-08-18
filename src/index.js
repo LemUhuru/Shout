431931d487d26934a5d22769365c1afee2b64af5
Lemuel <lemuel@Hans-Air.fios-router.home>
2018-08-18 14:39:07 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import registerServiceWorker from './config/registerServiceWorker'
+import registerServiceWorker from './registerServiceWorker'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import configStore from 'configStore'
